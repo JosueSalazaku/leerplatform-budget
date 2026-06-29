a5b758c54688823d758a6667cf53bab4f3175e88
Josué Salazaku <josuesalazaku@gmail.com>
2026-06-29 22:15:35 +0200

    Create footer and adjust layout padding

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Nav() {
   return (
     <div>
-      <ul>
+      <ul className="flex ">
         <li>
           <Link href="/">Home</Link>
         </li>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Nav from "./components/Nav";
+import { Footer } from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,11 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col"><Nav/>{children}</body>
+      <body className="min-h-screen flex flex-col px-12">
+        <Nav />
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
