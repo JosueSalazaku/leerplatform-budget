b8b12b616eadfc53cbccd663fed791dc30206b3f
Josué Salazaku <josuesalazaku@gmail.com>
2026-06-29 16:23:14 +0200

    Create nav component

---
+++ src/app/components/Nav.tsx
@@ -0,0 +1,9 @@
+export default function Nav() {
+    return <div>
+        <li>Home</li>
+        <li>Module 1</li>
+        <li>Module 2</li>
+        <li>Module 3</li>
+        <li>Module</li>
+    </div>
+}
